d82f865dbfde1879deb3cfe9e7328df9cdd0f0c2
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-12 16:06:19 +0200

    error comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
+
+    // state del formulario
+  const [busqueda, guardarBusqueda] = useState({ 
+      ciudad: '',
+      pais: ''
+  });
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+        if(consultar) {
+          const appId = 'c11a5e2033f0f29d1b7944c7a5563668';
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+  
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+  
+          guardarResultado(resultado);
+          guardarConsultar(false);
+
+          // Detecta si hubo resultados correctos en la consulta
+
+          if(resultado.cod === "404") {
+              guardarError(true);
+          } else {
+              guardarError(false);
+          }
+        }
+        
+    }
+    consultarAPI();
+    // eslint-disable-next-line
+  },[consultar]);
+
+  let componente;
+  if(error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima 
+                    resultado={resultado}
+                />
+  }
+
+
+
   return (
     <Fragment>
-    <Header 
-      titulo='☀️ Weather App 🌧'
-    />
-    <div className="contenedor-form">
-      <div className="container">
-        <div className="row">
-          <div className="col m6 s12">
-            <Formulario />
-          </div>
-          <div className="col m6 s12">
-            2
-          </div>
+        <Header 
+          titulo='Clima React App'
+        />
+
+        <div className="contenedor-form">
+            <div className="container">
+                <div className="row">
+                    <div className="col m6 s12">
+                        <Formulario 
+                          busqueda={busqueda}
+                          guardarBusqueda={guardarBusqueda}
+                          guardarConsultar={guardarConsultar}
+                        />
+                    </div>
+                    <div className="col m6 s12">
+                        {componente}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
     </Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = ({ titulo }) => {
-  return (
-    <nav>
-      <div className="nav-wrapper light-blue darken-2">
-        <a href="#!" className="brand-logo">{titulo}</a>
-      </div>
-    </nav>
-  );
-};
+const Header = ({titulo}) => {
+    return ( 
+        <nav>
+            <div className="nav-wrapper light-blue darken-2">
+                 <a href="#!" className="brand-logo">{titulo}</a>
+            </div>
+        </nav>
+     );
+}
 
+Header.propTypes = {
+    titulo: PropTypes.string.isRequired
+}
+ 
 export default Header;
